91e8bdf015fb5c0674720d142d54264ff5e2574b
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2024-10-08 22:50:58 +0530

    Update PasswordGeneratorReact.jsx

--- PasswordGenerator.jsx
+++ PasswordGenerator.jsx
@@ -1,14 +1,13 @@
-import { useRef } from "react";
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect ,useRef} from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [allowChar, setAllowChar] = useState(false);
   const [allowNum, setAllowNum] = useState(false);
   let [password, setPassword] = useState("");
-  // const btn =document.querySelector(".mess")
-  
-// useRef uses
+  const btn =document.querySelector(".mess")
+
+// useRef uses for reference of the Input Field.
 const passRef =useRef(null);
 
   // Callback Hook to memoize the changes in the variable.
@@ -23,9 +22,14 @@ const passRef =useRef(null);
       pass += str.charAt(char);
     }
     setPassword(pass); 
-  }, [length,allowChar,allowNum,setPassword,btn]);
+  }, [length,allowChar,allowNum,setPassword]);
 
-  
+  const reset =() =>{
+    passGenerator()
+    btn.innerHTML ="Copy";
+    btn.style.backgroundColor ="gray";
+
+  }
   // respond as per changes
   useEffect(() => {
     passGenerator();
@@ -37,8 +41,8 @@ const copytoClipboard =useCallback(()=>{
   passRef.current?.setSelectionRange(0,99);
 
   window.navigator.clipboard.writeText(password);
-  // btn.innerHTML = "Copied";
-  btn.style.backgroundColor ="grey"
+  btn.innerHTML = "Copied";
+  btn.style.backgroundColor ="olive";
 
 
 },[password,btn])
@@ -47,10 +51,10 @@ const copytoClipboard =useCallback(()=>{
   return (
     // Password Generation  code inside Fragment
     <>
-      <div className=" w-full  max-w-md mx-auto px-4 my-8  text-orange-500 rounded-lg bg-gray-700  ">
-        <div className="mt-4" >
-          <h1 className="text-center text-white ">Password Generator</h1>
-          <div className="flex shadow rounded-lg overflow-hidden mx-auto">
+      <div className="bg-olive h-full">
+        <div className="w-full  max-w-md mx-auto translate-y-28   px-4  text-orange-500 rounded-lg bg-gray-700" >
+          <h1 className="text-center text-white  p-6 font-bold underline text-2xl">Password Generator</h1>
+          <div className="flex shadow rounded-lg overflow-hidden py-4 mx-auto">
             <input
               type="text"
               value={password}
@@ -58,10 +62,10 @@ const copytoClipboard =useCallback(()=>{
               placeholder="Password"
               readOnly
               ref={passRef}
-              className="outline-none w-full py-1 px-2 rounded-xl "
+              className="outline-none w-full  px-2 py-1 rounded-xl "
             />
             
-            <button  onClick={copytoClipboard} className= "mess bg-blue-600  w-15 rounded-xl text-black">
+            <button  onClick={copytoClipboard} className= "mess bg-gray-600  w-20 rounded-xl text-black">
               Copy
             </button>
           </div>
@@ -96,10 +100,15 @@ const copytoClipboard =useCallback(()=>{
               Character
             </label>
           </div>
-          
+          <div className="flex justify-end m-3 p-4">
+          <button
+          className="bg-stone-800 w-20 p-2  text-white  rounded-md "
+          type="Reset" onClick={reset}> Reset</button>
+          </div>
+        
         </div>
       </div>
     </>
   );
 }
-export default App;
+export default App; 
